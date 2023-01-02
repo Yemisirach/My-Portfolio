4dcddacb5d824312895429baeaa733a892ebd682
Yemisirach <yemsrach3723@gmail.com>
2023-01-02 14:08:50 +0300

    Added script.js file on mobile menu

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("btn");
+const list = document.getElementById("list");
+
+list.style.display = "none";
+
+button.addEventListener("click", (event) => {
+  if (list.style.display == "none") {
+    list.style.display = "block";
+  } else {
+    list.style.display = "none";
+  }
+});
