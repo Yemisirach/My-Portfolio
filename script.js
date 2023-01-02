1111d6afa08664b3d91a5d8868b59766f6778e4d
lenkon <inoulenkon@gmail.com>
2023-01-02 20:52:05 +0530

    Add Mobile Menu

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const button = document.getElementById('btn');
 const list = document.getElementById('list');
+const menuShow = document.getElementById('menu-show');
 
 list.style.display = 'none';
 
-button.addEventListener('click', () => {
-  if (list.style.display === 'none') {
-    list.style.display = 'block';
-  } else {
-    list.style.display = 'none';
-  }
-});
+// button.addEventListener('click', () => {
+//   if (list.style.display === 'none') {
+//     list.style.display = 'block';
+//   } else {
+//     list.style.display = 'none';
+//   }
+// });
+
+const openMenu = () => {
+  menuShow.classList.add('show-menu');
+  // logo.classList.add('blur-filter');
+  // menu.classList.add('blur-filter');
+  // mainBody.classList.add('blur-filter');
+};
+
+button.addEventListener('click', openMenu);
