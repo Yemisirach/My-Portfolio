336f51889d6d73a9472722c941e314e6a3d2789d
lenkon <inoulenkon@gmail.com>
2023-01-05 14:56:25 +0530

    Add dynamic generation of works details

--- script.js
+++ script.js
@@ -41,52 +41,44 @@ const projectDetails = [
       "A daily selection of privately personalized reads; no accounts or sign-ups are required.",
     image: "./image/Snapshoot Portfolio.png",
     alt: "tonic project image",
-    companyRoles: { company: "Canopy", position: "Back End Dev", year: "2015" },
+    companyRoles: ["Canopy", "Back End Dev", "2015" ],
     skills: ["html", "css", "javascript"],
     liveLink: { link: "./index.html", text: "See live" },
-    sourceLink: {
-      link: "https://github.com/Yemisirach/MyPortfolio",
-      text: "See Source",
-    },
+    sourceLink: { link: "https://github.com/Yemisirach/MyPortfolio" },
     buttonText: "See Project",
+    id: 1
   },
+
   {
     title: "Multi_Post Stories",
     description:
       "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
     image: "./image/Snapshoot Portfolio (1).png",
     alt: "Multiple project image",
-    companyRoles: {
-      company: "FACEBOOK",
-      position: "Back End Dev",
-      year: "2015",
-    },
+    companyRoles: ["FACEBOOK", "Back End Dev", "2015"],
     skills: ["html", "css", "javascript"],
     liveLink: { link: "./index.html", text: "See live" },
     sourceLink: {
       link: "https://github.com/Yemisirach/MyPortfolio",
-      text: "See Source",
     },
     buttonText: "See Project",
+    flip: 1,
+    id: 2
   },
+
   {
     title: "Tonic",
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups are required.",
     image: "./image/desktop-version/Snapshoot Portfolio3.png",
     alt: "tonic project image",
-    companyRoles: {
-      company: "FACEBOOK 360",
-      position: "Back End Dev",
-      year: "2015",
-    },
+    companyRoles: [ "FACEBOOK 360", "Back End Dev", "2015"],
     skills: ["html", "css", "javascript"],
-    liveLink: { link: "../index.html", text: "See live" },
+    liveLink: { link: "./index.html", text: "See live" },
     sourceLink: {
-      link: "https://github.com/Yemisirach/MyPortfolio",
-      text: "See Source",
-    },
+      link: "https://github.com/Yemisirach/MyPortfolio" },
     buttonText: "See Project",
+    id: 3
   },
   {
     title: "Uber Navigation",
@@ -94,34 +86,30 @@ const projectDetails = [
       "A daily selection of privately personalized reads; no accounts or sign-ups are required.",
     image: "./image/desktop-version/Snapshoot Portfolio2.png",
     alt: "tonic project image",
-    companyRoles: {
-      company: "Uber",
-      position: "Lead Developer",
-      year: "2018",
-    },
+    companyRoles: ["Uber", "Lead Developer", "2018"],
     skills: ["html", "css", "javascript"],
-    liveLink: { link: "../index.html", text: "See live" },
-    sourceLink: {
-      link: "https://github.com/Yemisirach/MyPortfolio",
-      text: "See Source",
-    },
+    liveLink: { link: "./index.html", text: "See live" },
+    sourceLink: { link: "https://github.com/Yemisirach/MyPortfolio" },
     buttonText: "See Project",
+    flip: 1,
+    id: 4
   },
 ];
-const project = document.getElementById("grid-container");
+
+// const project = document.getElementById("grid-container");
 // document.getElementById("grid-container").innerHTML = "I have changed!";
-const projectcad = document.querySelectorAll("snapshoot");
+//const projectcad = document.querySelectorAll("snapshoot");
 // console.log(projectcad);
 // const snapshoot = document.querySelector('.snapshoot_revers');
 // const cadDaital = document.querySelector('.profile');
 // const langList = document.querySelector('.skill');
 
 const creatProjectDetails = (item) => {
-  const snapshoot = document.createElement("div");
-  if (item.flip == 1) {
-    snapshoot.classList.add("grid-container, snapshoot_revers ");
+  const snapshootDetails = document.createElement("div");
+  if (item.flip === 1) {
+    snapshootDetails.classList.add("snapshoot", "snapshoot_revers");
   } else {
-    snapshoot.classList.add("grid-container");
+    snapshootDetails.classList.add("snapshoot");
   }
 
   const roles = item.companyRoles.map((value, index) => {
@@ -130,6 +118,10 @@ const creatProjectDetails = (item) => {
     }
     return `<span class="card-canopy">${value}</span>`;
   });
+
+  const skillsList = item.skills.map(
+    (count) => `<li class="card-tools">${count}</li>`,
+  );
 
   const innerHTML = `<img src="${item.image}" alt="${item.alt}">
   <div class="card-sizing">
@@ -147,7 +139,121 @@ const creatProjectDetails = (item) => {
       } class="card-button popup-card-button">See Project</button>
     </div>
   </div>`;
-  snapshoot.innerHTML = innerHTML;
-  cardWorksWrapper.appendChild(card);
+  
+  snapshootDetails.innerHTML = innerHTML;
+  cardWorksWrapper.appendChild(snapshootDetails);
 };
+
 projectDetails.forEach((item) => creatProjectDetails(item));
+
+const popupDetails = (item) => {
+  const roles = item.companyRoles.map((value, index) => {
+    if (index !== 0) {
+      return `<div class="card-bullet-separator"></div> <span class="card-canopy card-canopy-details">${value}</span>`;
+    }
+    return `<span class="card-canopy">${value}</span>`;
+  });
+
+  const skillsList = item.skills.map(
+    (count) => `<li class="card-tools">${count}</li>`,
+  );
+
+  const innerHtml = `<div id="popup-card-container"></div>
+  <section id="popup-card-section">
+    <article class="popup-card">
+      <header class="popup-details-header">
+        <nav class="popup-card-details">
+          <h2 class="card-title-text">${item.title}</h2>
+          <button id="popup-close-button" class="popup-close-button">
+            <img src="./image/Popup-close-Icon.png" alt="popup close icon">
+          </button>
+        </nav>
+        <div class="card-list">
+          ${roles.join('')}
+        </div>
+      </header>
+      <section class="popup-details-container">
+        
+          <img src="${item.image}" alt="${item.alt}">
+        
+        <div class="popup-card-details-container"> 
+          <p class="card-main-text popup-card-description">${popupCardDescription}</p>
+          <div class="popup-tools-sizing">
+            <ul class="card-tools-list">
+              ${skillsList.join('')}
+            </ul>
+            <div class="popup-card-separator"></div>
+            <div class="popup-card-links">
+              <a href="${
+  item.liveLink.link
+}" target="_blank" class="card-button popup-button">
+                <span>See live</span>
+                <img src="./image/live-link-Icon.png" alt="live link icon">
+              </a>
+              <a href="${
+  item.sourceLink.link
+}" target="_blank" class="card-button popup-button">
+                <span>See source</span>
+                <img src="./image/github-Vector.png" alt="github link icon">
+              </a>
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  </section>`;
+  return innerHtml;
+};
+
+const createPopup = (button, callback = () => null) => {
+  const id = parseInt(button.getAttribute('card-id'), 10);
+  const data = projectDetails.filter((item) => item.id === id);
+  logo.classList.add("blur-filter");
+  home.classList.add("blur-filter");
+  button.classList.add("blur-filter");
+  mainBody.classList.add("blur-filter");
+  document.getElementById('popup-project-card-section').innerHTML = popupDetails(data[0]);
+  return callback();
+};
+
+// Close popup
+const closePopup = () => {
+  const popupCardContainer = document.getElementById('popup-card-container');
+  const popupCloseButton = document.getElementById('popup-close-button');
+
+  logo.classList.add('blur-filter');
+  menu.classList.add('blur-filter');
+  mainBody.classList.add('blur-filter');
+
+  popupCloseButton.addEventListener('click', () => {
+    document.getElementById('popup-project-card-section').innerHTML = '';
+    document.body.style.margin = '';
+    document.body.style.overflow = '';
+    logo.classList.remove('blur-filter');
+    menu.classList.remove('blur-filter');
+    mainBody.classList.remove('blur-filter');
+  });
+  popupCardContainer.addEventListener('click', () => {
+    document.getElementById('popup-project-card-section').innerHTML = '';
+    document.body.style.margin = '';
+    document.body.style.overflow = '';
+  });
+  for (let i = 0; i < popupCardContainer.childNodes.length; i += 1) {
+    popupCardContainer.childNodes[i].addEventListener('click', (event) => {
+      event.stopPropagation();
+    });
+  }
+};
+
+const cardButton = document.querySelectorAll('.popup-card-button');
+cardButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    document.body.style.overflow = 'hidden';
+    createPopup(button, closePopup);
+  });
+});
+
+
+
+
